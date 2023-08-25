3f1e4eb3c3ed83fc78229cb0f0406f0ba8bd0970
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-08-25 22:48:12 +0600

    input fetch

--- src/Component/Token.jsx
+++ src/Component/Token.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
 const Token = () => {
+    const [data, setData]= useState({})
+  const [token, setToken] = useState('')
+  const url =`https://api.dexscreener.com/latest/dex/tokens/${token}`
+    
+  const handlesearch=(event)=>
+     {if(event.key === 'Enter')
+    
+   { fetch(url)
+    .then(res=> res.json())
+    .then(data=> setData(data))
+    setToken('')
+  }
+      console.log(data);
+
+  }
     return (
         <div>
-            <h1>token</h1>
+           
+                <input style={{borderRadius:"50px", marginRight:"65vw"}} className="w-96 input input-bordered mt-20 h-12  text-center text-lg" type="text"  id="" placeholder='serach'
+      value={token} 
+      onChange={event => setToken(event.target.value)}
+      onKeyPress={handlesearch} 
+                />
+                
+           
         </div>
     );
 };
